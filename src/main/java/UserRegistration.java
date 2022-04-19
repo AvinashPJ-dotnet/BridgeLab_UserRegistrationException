63ab051219d03668a42053ed2d884bbce146df2c
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-19 12:04:26 +0530

    Added: validate password function and followed rule1

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -8,7 +8,7 @@ public class UserRegistration {
     public void addUser() {
         int choice = 0;
         do {
-            System.out.println("1. First Name\n2. Last Name\n3. Email\n4. Phone Number\n0. Exit");
+            System.out.println("1. First Name\n2. Last Name\n3. Email\n4. Phone Number\n5. Password\n0. Exit");
             System.out.println("Enter choice");
             choice = scanner.nextInt();
             switch (choice) {
@@ -27,6 +27,10 @@ public class UserRegistration {
                 case 4:
                     String phoneNumber = stringInput("Enter phone number");
                     addPhoneNumber(phoneNumber);
+                    break;
+                case 5:
+                    String password = stringInput("Enter Password");
+                    addPassword(password);
                     break;
             }
         } while (choice != 0);
@@ -78,6 +82,17 @@ public class UserRegistration {
             return true;
         } else {
             System.out.println("Please check Phone Number");
+            return false;
+        }
+    }
+
+    public boolean addPassword(String password) {
+        boolean isPassword = Pattern.matches(UserDetailsRegexPattern.PASSWORD, password);
+        if (isPassword) {
+            userDetails.setPassword(password);
+            return true;
+        } else {
+            System.out.println("Please check Password");
             return false;
         }
     }
